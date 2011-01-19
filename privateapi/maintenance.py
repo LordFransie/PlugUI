759cf36d493c3d1b0eecebd14178348f577a8266
mrsteveman1 <mrsteveman1@gmail.com>
2011-01-19 06:32:13 -0600

    fix news feed location, add method for pinging the mother ship

--- privateapi/maintenance.py
+++ privateapi/maintenance.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python 
 # coding: utf8
 
-import shlex, subprocess, re, socket, os
+import shlex, subprocess, re, socket, os, httplib, urllib
 import feedparser
 from home.models import PlugappsNewsEntry
 from system.models import MaintenanceStats
 import privateapi.pacman
-
+import privateapi.core
 
 
 def update_plugapps_news():
 	PlugappsNewsEntry.objects.all().delete()
-	plugappsfeed = feedparser.parse("http://dev.plugapps.com/category/news/feed/")
+	plugappsfeed = feedparser.parse("http://dev2.plugapps.com/category/news/feed/")
 	feed_items = plugappsfeed['entries']
 	for story in feed_items:
 		item = PlugappsNewsEntry()
@@ -21,8 +21,18 @@ def update_plugapps_news():
 		item.link = story['link']
 		item.save()
 
+def update_plug_location():
+	currentmac = privateapi.core.getethermac()
+	currentip = privateapi.core.getcurrentip()
+	params = urllib.urlencode( { 'plugid': currentmac, 'localip': currentip } )
+	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+	conn = httplib.HTTPConnection("plugfinder.appspot.com:80")
+	conn.request("POST", "/", params, headers)
+	conn.close()
+
 def run_maintenance():
 	update_plugapps_news()
+	update_plug_location()
 	if privateapi.pacman.check():
 		privateapi.pacman.list_upgrades()
 	maintenancestats = MaintenanceStats()
